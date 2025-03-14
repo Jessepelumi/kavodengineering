9bbdc66a7ca5305887fa1a1ad1419f7a3c8ac7c8
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-14 07:27:58 +0100

    modified parent overscroll behaviour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased overscroll-none">{children}</body>
     </html>
   );
 }
