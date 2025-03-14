a0fdb3072e3028143e353e2e92a7759b995b37b1
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-14 09:24:20 +0100

    made the button transition faster

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
           <Link href="/">Contact</Link>
         </nav>
       </div>
-      <button className="relative overflow-hidden text-[14px] bg-secondary-500 py-2 px-4 rounded-3xl transition-all duration-500 ease-in-out before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-brand-500 before:transition-all before:duration-500 before:ease-in-out hover:before:w-full hover:text-base-white">
+      <button className="relative overflow-hidden text-[14px] bg-secondary-500 py-2 px-4 rounded-3xl transition-all duration-500 ease-in-out before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-brand-500 before:transition-all before:duration-250 before:ease-in-out hover:before:w-full hover:text-base-white">
         <span className="relative z-10">Coming soon</span>
       </button>
     </aside>
