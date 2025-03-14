ebd8c341f6b3ac28eb18208c75460fd799382993
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-14 07:58:15 +0100

    waitlist announcement banner (not fully done)

---
+++ src/components/custom/waitlistBanner.tsx
@@ -0,0 +1,21 @@
+export const WaitlistBanner = () => {
+  return (
+    <aside className="bg-brand-700 text-base-white text-[12px] p-2.5 text-center">
+      KAVOD WEBSITE LAUNCH IS COMING ON APRIL 20TH, 2025. JOIN THE WAITLIST! 🎉
+    </aside>
+  );
+};
+
+// export const WaitlistBanner = () => {
+//   return (
+//     <aside className="bg-brand-700 text-base-white text-[12px] p-2.5 flex justify-center items-center gap-2.5">
+//       <span>
+//         KAVOD WEBSITE LAUNCH IS COMING ON APRIL 20TH, 2025. JOIN THE WAITLIST!
+//         🎉
+//       </span>
+//       <button>
+//         <img src="/svg/X.svg" alt="cancel icon" className="w-4 h-4" />
+//       </button>
+//     </aside>
+//   );
+// };
