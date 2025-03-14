18406f489b1f915794e485855139e8d94e7092a0
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-14 18:28:50 +0100

    updated the waitlist page

--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -34,8 +34,24 @@ export default function WaitlistPage() {
         </div>
       </section> */}
 
-      <section className="h-full bg-base-black bg-blend-overlay">
+      <section className="relative h-full flex justify-center items-center text-base-white">
         <BackgroundCarousel />
+
+        <div className="absolute z-10 inset-0 bg-gradient-to-b from-transparent via-base-black via-85% to-base-black"></div>
+
+        <div className="absolute z-20 flex flex-col justify-center items-center text-center gap-4 px-6">
+          <h1 className="text-[42px] sm:text-[52px] md:text-[64px] leading-[50px] md:leading-[65px] font-bold max-w-[95%] md:max-w-[70%] lg:max-w-[50%]">
+            Be the First to Experience KAVOD
+          </h1>
+          <p className="text-[16px] max-w-[85%] md:max-w-[70%] lg:max-w-[35%]">
+            Kavod Engineering Services offers expert welding inspection,
+            coatings, and corrosion control, quality assurance, materials
+            testing, and CWI training. Join our waitlist and be the first to
+            access our full website and services.
+          </p>
+
+          <WaitlistForm />
+        </div>
       </section>
     </div>
   );
