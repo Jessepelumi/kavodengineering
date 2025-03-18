c147d51a280437c97017168d882ae668a6a77ae1
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-18 17:02:05 +0100

    added header component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,9 @@
+import { HomeHeader } from "@/components/custom/homeHeader";
+
 export default function Home() {
-  <div className="h-[50vh] w-[100vw] bg-base-black">
-    <h1>Home Page</h1>
-  </div>;
+  return (
+    <div>
+      <HomeHeader />
+    </div>
+  );
 }
