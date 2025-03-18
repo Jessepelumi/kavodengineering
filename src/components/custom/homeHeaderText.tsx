f3f71c83daca89eb2fc203d06237ec532bcd41bf
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-18 17:00:09 +0100

    text body for the home header

---
+++ src/components/custom/homeHeaderText.tsx
@@ -0,0 +1,13 @@
+export const HomeHeaderText = () => {
+  return (
+    <article className="z-20 flex flex-col items-start gap-4">
+      <h1 className="text-[36px] leading-[42px] sm:text-[52px] md:text-[64px]  md:leading-[68px] font-bold max-w-[95%] md:max-w-[80%] lg:max-w-[70%]">
+        Engineering Quality, Ensuring Integrity
+      </h1>
+      <p className="text-[14px] md:text-[16px] leading-[20px] md:leading-[24px] max-w-[80%] md:max-w-[60%] lg:max-w-[55%] xl:max-w-[45%]">
+        Precision engineering, coating, welding inspection, and quality
+        assurance for safer, stronger infrastructure.
+      </p>
+    </article>
+  );
+};
