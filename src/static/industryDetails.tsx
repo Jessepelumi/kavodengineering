24ad08b30bf3f9cfc75f32ccd8cb167a7b5e76d8
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-21 17:29:04 +0100

    json data for industries served details

---
+++ src/static/industryDetails.tsx
@@ -0,0 +1,44 @@
+export type Industry = {
+  industry: string;
+  description: string;
+  icon: string;
+};
+
+export const IndustryDetails: Industry[] = [
+  {
+    industry: "Construction & Maintenance",
+    description:
+      "KAVOD provides expert welding inspections, materials testing, and failure analysis to ensure buildings, bridges, and infrastructure projects meet the highest safety and durability standards.",
+    icon: "/svg/Wrench.svg",
+  },
+  {
+    industry: "Oil & Gas",
+    description:
+      "KAVOD helps oil and gas companies maintain safety and regulatory compliance through rigorous testing, failure analysis, and materials evaluations for pipelines, drilling sites, and refineries.",
+    icon: "/svg/Drop.svg",
+  },
+  {
+    industry: "Manufacturing & Heavy Industry",
+    description:
+      "From machinery components to large-scale metal structures, KAVOD's expertise in materials testing and inspection ensures manufacturing processes remain efficient, compliant, and free from defects.",
+    icon: "/svg/Buildings.svg",
+  },
+  {
+    industry: "Transportation & Infrastructure",
+    description:
+      "KAVOD enhances the safety and longevity of roads, bridges, and transit systems through expert welding inspections, material testing, and failure analysis, ensuring resilience in every structure.",
+    icon: "/svg/Car.svg",
+  },
+  {
+    industry: "Aerospace",
+    description:
+      "Flight demands perfection. KAVOD provides rigorous testing, material evaluations, and failure analysis to enhance aircraft safety, reliability, and compliance, ensuring optimal performance from takeoff to touchdown.",
+    icon: "/svg/AirplaneTakeoff.svg",
+  },
+  {
+    industry: "Nuclear & Power Generation",
+    description:
+      "Energy infrastructure must be built to last. KAVOD delivers specialized testing and inspection to maintain safety, efficiency, and compliance in nuclear and power facilities, securing the future of energy production.",
+    icon: "/svg/ChargingStation.svg",
+  },
+];
