407a41c9f75ee378ff9e26ddf9903b8a2352338e
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-28 13:33:33 +0100

    done with navbar structure

--- src/components/views/navbar.tsx
+++ src/components/views/navbar.tsx
@@ -4,11 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
+import { CustomButton } from "../custom/customButton";
 
+// static navigation bar component
 export const Navbar = () => {
   const pathname = usePathname();
   const [isMobileNavbarOpen, setIsMobileNavbarOpen] = useState(false);
 
+  // function to handle scrolling into the about section
   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
     const section = document.getElementById("about");
@@ -20,50 +23,38 @@ export const Navbar = () => {
   return (
     <>
       <aside className="bg-base-white px-5 flex justify-between items-center py-3 lg:px-10">
-        <div className="flex items-center gap-14">
-          <Link href="/">
-            <Image
-              src="/images/KESMainLogo.png"
-              alt="Kavod Engineering Main Logo"
-              width={128}
-              height={64}
-              priority
-              className="h-auto w-auto"
-            />
-          </Link>
-          <nav className="hidden md:flex justify-between space-x-7 text-gray-600 font-semibold text-[14px]">
-            <a
-              href=""
-              onClick={handleScroll}
-              className={`hover:text-brand-700 hover:font-semibold ${
-                pathname === "" ? "text-brand-700 font-semibold" : ""
-              }`}
-            >
+        <Link href="/">
+          <Image
+            src="/images/KESMainLogo.png"
+            alt="Kavod Engineering Main Logo"
+            width={128}
+            height={64}
+            priority
+            className="h-auto w-auto"
+          />
+        </Link>
+
+        <div className="flex items-center space-x-7">
+          <nav className="hidden md:flex justify-between space-x-7 text-gray-600 text-[14px]">
+            <a href="" onClick={handleScroll} className="hover:text-brand-700">
               About us
             </a>
+
             <Link
               href="/services"
-              className={`hover:text-brand-700 hover:font-semibold ${
-                pathname === "/services" ? "text-brand-700 font-semibold" : ""
+              className={`hover:text-brand-700 ${
+                pathname === "/services" ? "text-brand-700" : ""
               }`}
             >
               Services
             </Link>
-            <Link
-              href="/contact"
-              className={`hover:text-brand-700 hover:font-semibold ${
-                pathname === "/contact" ? "text-brand-700 font-semibold" : ""
-              }`}
-            >
-              Contact
-            </Link>
           </nav>
+
+          <CustomButton className="hidden md:block">
+            <Link href="/contact">Get a consultation</Link>
+          </CustomButton>
         </div>
-        <button className="hidden md:block relative overflow-hidden text-[14px] bg-secondary-500 py-2 px-4 rounded-3xl transition-all duration-500 ease-in-out before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-brand-500 before:transition-all before:duration-250 before:ease-in-out hover:before:w-full hover:text-base-white">
-          <Link href="/contact" className="relative z-10">
-            Get a consultation
-          </Link>
-        </button>
+
         <button
           onClick={() => setIsMobileNavbarOpen(!isMobileNavbarOpen)}
           className="visible md:hidden"
@@ -85,6 +76,7 @@ export const Navbar = () => {
   );
 };
 
+// collapsible mobile navigation component
 interface MobileNavbarProps {
   isOpen: boolean;
   closeMenu: () => void;
@@ -104,7 +96,7 @@ export const MobileNavbar = ({ isOpen, closeMenu }: MobileNavbarProps) => {
 
   return (
     <section
-      className={`fixed top-0 w-full h-dvh bg-base-white py-10 px-6 flex flex-col justify-between items-end md:hidden ${
+      className={`fixed top-0 w-full h-dvh bg-base-white py-10 px-6 flex flex-col justify-between md:hidden ${
         isOpen ? "-translate-x-0" : "translate-x-full"
       } transform transition-transform duration-400`}
     >
@@ -120,10 +112,12 @@ export const MobileNavbar = ({ isOpen, closeMenu }: MobileNavbarProps) => {
               className="h-auto w-auto"
             />
           </Link>
+
           <button onClick={closeMenu} className="">
             <Image src="svg/X.svg" alt="Cancle Button" width={24} height={24} />
           </button>
         </div>
+
         <nav>
           <a
             href=""
@@ -152,28 +146,14 @@ export const MobileNavbar = ({ isOpen, closeMenu }: MobileNavbarProps) => {
               height={16}
             />
           </Link>
-
-          <Link
-            href="/contact"
-            className="flex justify-between items-center border-b-[0.5px] py-5 pr-2.5"
-            onClick={closeMenu}
-          >
-            Contact
-            <Image
-              src="/svg/ArrowUpRight.svg"
-              alt="Navigate to Services"
-              width={16}
-              height={16}
-            />
-          </Link>
         </nav>
       </div>
 
-      <button className="w-fit relative overflow-hidden text-[14px] bg-secondary-500 py-2 px-4 rounded-3xl transition-all duration-500 ease-in-out before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-brand-500 before:transition-all before:duration-250 before:ease-in-out hover:before:w-full hover:text-base-white">
-        <Link href="/contact" className="relative z-10" onClick={closeMenu}>
+      <CustomButton className="py-3.5">
+        <Link href="/contact" onClick={closeMenu}>
           Get a consultation
         </Link>
-      </button>
+      </CustomButton>
     </section>
   );
 };
