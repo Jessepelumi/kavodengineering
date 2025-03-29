fe9ac795112eefd1844eb33a01f4acf9f1acb155
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-03-29 05:08:17 +0100

    card component to hold value proposition details

---
+++ src/components/custom/valuePropositionCard.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import clsx from "clsx";
+
+interface ValuePropositionCardProps {
+  image: string;
+  imageClassName?: string;
+  title: string;
+  description: string;
+}
+
+export const ValuePropositionCard = ({
+  image,
+  imageClassName,
+  title,
+  description,
+}: ValuePropositionCardProps) => {
+  return (
+    <div className="h-[320px] md:h-[280px] lg:h-[300px] bg-background flex flex-col justify-between p-4">
+      <div className={clsx("relative", imageClassName)}>
+        <Image
+          src={`/svg/${image}`}
+          alt={`Kavod Provides ${title}`}
+          fill
+          className="object-cover object-center"
+        />
+      </div>
+      <div>
+        <h3 className="text-[20px] font-medium">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
