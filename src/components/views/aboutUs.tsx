e6dfd2f5d3729b189dd149a1f40e0752b26c24a3
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-02 18:03:23 +0100

    modified about us and industries served views

--- src/components/views/aboutUs.tsx
+++ src/components/views/aboutUs.tsx
@@ -1,53 +1,32 @@
-import Image from "next/image";
+import { SectionHeading } from "../custom/sectionHeading";
+import { VisionMissionText } from "../custom/visionMissionText";
 
 export const AboutUs = () => {
   return (
-    <div id="about" className="flex flex-col xl:flex-row gap-10 xl:gap-14 py-10 px-5 md:py-16 md:px-10">
+    <div
+      id="about"
+      className="flex flex-col xl:flex-row gap-10 xl:gap-14 py-10 px-5 md:py-16 md:px-10"
+    >
       <section className="xl:w-1/2">
-        <h4 className="text-secondary-600 pb-2">ABOUT US</h4>
-        <h3 className="pb-2 text-[30px] md:text-[36px] xl:max-w-[60%] md:max-w-[50%] leading-[36px] md:leading-[42px]">
-          Experts in Welding & Inspection
-        </h3>
-        <p className="xl:max-w-[80%]">
-          At KAVOD, we combine deep expertise in welding engineering, material
-          science, and quality assurance to help clients deliver safer,
-          stronger, and more reliable infrastructure.
-        </p>
+        <SectionHeading
+          section="about us"
+          title="Experts in Welding & Inspection"
+          paragraph="At KAVOD, we combine deep expertise in welding engineering, material science, and quality assurance to help clients deliver safer, stronger, and more reliable infrastructure."
+        />
       </section>
+
       <section className="xl:w-1/2 flex flex-col gap-10 xl:gap-14">
-        <article className="">
-          <div className="flex gap-2 items-center pb-5 text-[20px] md:text-[24px]">
-            <Image
-              src="/svg/Target.svg"
-              alt="Kavod Mission"
-              width={24}
-              height={24}
-            />
-            Our Mission
-          </div>
-          <p>
-            To deliver expert inspection, quality assurance, and welding testing
-            solutions that ensure the integrity safety, and reliability of
-            critical infrastructure &mdash; empowering our clients to meet
-            industry standard and exceed performance expectations.
-          </p>
-        </article>
-        <article>
-          <div className="flex gap-2 items-center pb-5 text-[20px] md:text-[24px]">
-            <Image
-              src="/svg/Eye.svg"
-              alt="Kavod Mission"
-              width={24}
-              height={24}
-            />
-            Our Vision
-          </div>
-          <p>
-            To be a trusted leader in welding engineering and quality assurance
-            services, setting the benchmark for technical excellence,
-            exceptional quality, and innovation and client-focused solutions.
-          </p>
-        </article>
+        <VisionMissionText
+          icon="Target.svg"
+          title="Our Mission"
+          paragraph="Our mission at Kavod Engineering Services is to deliver industry-leading quality assurance, inspection, and consulting services that enhance safety, compliance, and operational excellence. We strive to build long-term partnership with our clients by upholding the highest standards of integrity, professionalism, and technical expertise."
+        />
+
+        <VisionMissionText
+          icon="Eye.svg"
+          title="Our Vision"
+          paragraph="To be a trusted leader in quality assurance and materials consulting, setting new benchmarks for excellence in the structural and manufacturing industries Through innovation, continuous improvement, and a commitment to ethical engineering practices, we aim to contribute to a safer, more efficient, and high-quality built environment."
+        />
       </section>
     </div>
   );
