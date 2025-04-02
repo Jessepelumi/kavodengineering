31d9e3b70419ea4d05234e89867fd7561919ef23
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-02 18:03:55 +0100

    added hero

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,13 @@
+import { ServiceContactHeader } from "@/components/views/serviceContactHeader";
+
 export default function Services() {
   return (
-    <div>
-      <h1>Services</h1>
-    </div>
+    <>
+      <ServiceContactHeader
+        section="our services"
+        title="Our Expertise, Your Assurance"
+        description="At KAVOD, we specialize in advanced material testing, inspections, and failure analysis to help businesses maintain the highest safety, durability, and compliance standards."
+      />
+    </>
   );
 }
