868340d0cb7aa5099a388403006452b6cf072af6
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-02 18:03:45 +0100

    added hero and contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,18 @@
+import ContactForm from "@/components/views/contactForm";
+import { ServiceContactHeader } from "@/components/views/serviceContactHeader";
+
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <>
+      <ServiceContactHeader
+        section="contact us"
+        title="Need a Consultation?"
+        description="Have questions or need expert assistance? Reach out to us, and our team will be happy to assist you."
+      />
+
+      <div className=" flex justify-center items-center h-screen px-[5%] lg:px-[10%] xl:px-[15%]">
+        <ContactForm />
+      </div>
+    </>
   );
 }
