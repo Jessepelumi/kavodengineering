5687b1629d049192282b1a06a88774e9d9d146d1
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-02 18:04:24 +0100

    changed container tag

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,15 @@
 import { AboutUs } from "@/components/views/aboutUs";
 import { HomeHeader } from "@/components/views/homeHeader";
 import { IndustriesServed } from "@/components/views/industriesServed";
+import { OurServices } from "@/components/views/ourServices";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <HomeHeader />
       <AboutUs />
       <IndustriesServed />
-    </div>
+      <OurServices />
+    </>
   );
 }
