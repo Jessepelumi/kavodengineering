bbd9c215aef965cd406e3e395db15b0f73b1c31d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-04 09:08:39 +0100

    modified image array - removed the third image

--- src/components/custom/backgroundCarouselWithControls.tsx
+++ src/components/custom/backgroundCarouselWithControls.tsx
@@ -15,7 +15,6 @@ interface BackgroundCarouselProps {
 const images = [
   "/images/image1.jpg",
   "/images/image2.jpg",
-  "/images/image3.jpg",
   "/images/image4.jpg",
   "/images/image5.jpg",
   "/images/image6.jpg",
