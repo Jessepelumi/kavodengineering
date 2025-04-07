734df429ff923cece6a8231cec00dd7541774a63
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-07 19:45:22 +0100

    created a string file to store the details of the services

---
+++ src/static/servicesDetails.tsx
@@ -0,0 +1,17 @@
+import test from "node:test";
+
+export const ServicesDetails = {
+  main: "At KAVOD, we specialize in advanced material testing, inspections, and failure analysis to help businesses maintain the highest safety, durability, and compliance standards.",
+
+  engineeringConsulting:
+    "Achieving strong, durable, and defect-free welds requires expert guidance. Our engineering consulting services provide in-depth analysis, process optimization, and technical support to enhance welding quality and efficiency. We assist in welding procedure development, welder qualification, failure investigation, and process improvements, ensuring compliance with industry standards.",
+
+  testingAndAnalysis:
+    "Our comprehensive testing and analysis service cover mechanical testing, chemical analysis, non-destructive testing (NDT), and metallurgical evaluation to assess strength, composition, and durability. We help industries prevent failures, improve product performance, and comply with safety regulations.",
+
+  inspectionServices:
+    "Our inspection services ensure the integrity and quality of materials and welds. We conduct thorough inspections using advanced techniques such as visual inspection, ultrasonic testing, radiographic testing, and magnetic particle testing. Our certified inspectors provide detailed reports and recommendations to help you maintain compliance with industry standards and regulations.",
+
+  technicalTraining:
+    "Building a skilled workforce is essential for success in the welding industry. Our technical training programs cover various aspects of welding, including welding processes, safety practices, and quality control. We offer hands-on training, workshops, and seminars to equip your team with the knowledge and skills needed to excel in their roles.",
+};
