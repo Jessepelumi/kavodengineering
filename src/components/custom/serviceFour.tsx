a3210b6365d6f95e7a60e97da40ae02f61c5b3d8
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-09 19:29:48 +0100

    kavod service sections

---
+++ src/components/custom/serviceFour.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+import { SectionHeading } from "@/components/custom/sectionHeading";
+import { ServicesDetails } from "@/static/servicesDetails";
+
+export const ServiceFour = () => {
+  return (
+    <section className="space-y-5 md:space-y-10">
+      <SectionHeading section="service 4" title="Technical Training" />
+
+      <div className="relative w-full h-[300px] md:h-[457px]">
+        <Image
+          src="/images/serviceImageFour.jpeg"
+          alt=""
+          fill
+          className="object-cover object-center"
+        />
+      </div>
+
+      <p>{ServicesDetails.technicalTraining}</p>
+    </section>
+  );
+};
---
+++ src/components/custom/serviceThree.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+import { SectionHeading } from "@/components/custom/sectionHeading";
+import { ServicesDetails } from "@/static/servicesDetails";
+
+export const ServiceThree = () => {
+  return (
+    <section className="space-y-5 md:space-y-10">
+      <SectionHeading section="service 3" title="Inspection Services" />
+
+      <div className="grid grid-cols-2 gap-4">
+        <div className="relative w-full h-[300px] md:h-[457px]">
+          <Image
+            src="/images/serviceImageThree.jpeg"
+            alt=""
+            fill
+            className="object-cover object-center"
+          />
+        </div>
+
+        <div className="relative w-full h-[300px] md:h-[457px]">
+          <Image
+            src="/images/serviceImageFive.jpeg"
+            alt=""
+            fill
+            className="object-cover object-center"
+          />
+        </div>
+      </div>
+
+      <p>{ServicesDetails.inspectionServices}</p>
+    </section>
+  );
+};
