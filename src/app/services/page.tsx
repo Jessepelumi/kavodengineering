28b4cc32361141e6da08ea97e410237a0db8de83
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-09 19:30:32 +0100

    modified imports

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,11 @@
+import { IndustriesGrid } from "@/components/custom/industriesGrid";
+import { SectionHeading } from "@/components/custom/sectionHeading";
+import { SendMessageCard } from "@/components/custom/sendMessageCard";
 import { ServiceContactHeader } from "@/components/views/serviceContactHeader";
+import { ServiceOne } from "@/components/custom/serviceOne";
+import { ServiceTwo } from "@/components/custom/serviceTwo";
+import { ServiceThree } from "@/components/custom/serviceThree";
+import { ServiceFour } from "@/components/custom/serviceFour";
 
 export default function Services() {
   return (
@@ -8,6 +15,29 @@ export default function Services() {
         title="Our Expertise, Your Assurance"
         description="At KAVOD, we specialize in advanced material testing, inspections, and failure analysis to help businesses maintain the highest safety, durability, and compliance standards."
       />
+
+      <div className="flex flex-col gap-15 md:gap-20 py-16 px-5 md:px-10">
+        <ServiceOne />
+        <ServiceTwo />
+        <ServiceThree />
+        <ServiceFour />
+      </div>
+
+      <div className="bg-brand-700 flex flex-col gap-10 py-16 px-5 md:px-10 items-center">
+        <SectionHeading
+          variant="white"
+          section="industries served"
+          title="We cover materials engineering across these industries"
+          paragraph="From welding inspection to failure analysis, we provide precision-engineered solutions across several industries."
+          className="items-center text-center"
+        />
+
+        <IndustriesGrid />
+      </div>
+
+      <div className="flex flex-col gap-10 py-16 px-5 md:px-10">
+        <SendMessageCard />
+      </div>
     </>
   );
 }
