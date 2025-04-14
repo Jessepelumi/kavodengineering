134d9513516d78c856eb84c5b6875c1a3450a46f
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-14 13:04:17 +0100

    created new service card and modified service content

--- src/content/servicesContent.tsx
+++ src/content/servicesContent.tsx
@@ -1,13 +1,34 @@
-export const ServicesContent = {
-  engineeringConsulting:
-    "Achieving strong, durable, and defect-free welds requires expert guidance. Our engineering consulting services provide in-depth analysis, process optimization, and technical support to enhance welding quality and efficiency. We assist in welding procedure development, welder qualification, failure investigation, and process improvements, ensuring compliance with industry standards.",
-
-  testingAndAnalysis:
-    "Our comprehensive testing and analysis service cover mechanical testing, chemical analysis, non-destructive testing (NDT), and metallurgical evaluation to assess strength, composition, and durability. We help industries prevent failures, improve product performance, and comply with safety regulations.",
-
-  inspectionServices:
-    "Our inspection services ensure the integrity and quality of materials and welds. We conduct thorough inspections using advanced techniques such as visual inspection, ultrasonic testing, radiographic testing, and magnetic particle testing. Our certified inspectors provide detailed reports and recommendations to help you maintain compliance with industry standards and regulations.",
-
-  technicalTraining:
-    "Building a skilled workforce is essential for success in the welding industry. Our technical training programs cover various aspects of welding, including welding processes, safety practices, and quality control. We offer hands-on training, workshops, and seminars to equip your team with the knowledge and skills needed to excel in their roles.",
-};
+export const ServicesContent = [
+  {
+    id: 1,
+    image: "expertGuidance.jpeg",
+    section: "expert guidance",
+    title: "Engineering Consulting",
+    description:
+      "Achieving strong, durable, and defect-free welds requires expert guidance. Our engineering consulting services provide in-depth analysis, process optimization, and technical support to enhance welding quality and efficiency. We assist in welding procedure development, welder qualification, failure investigations, and process improvements, ensuring compliance with industry standards.",
+  },
+  {
+    id: 2,
+    image: "precisionTesting.jpeg",
+    section: "precision testing",
+    title: "Testing & Analysis",
+    description:
+      "Our comprehensive testing and analysis services cover mechanical testing, chemical analysis, non-destructive testing (NDT), and metallurgical evaluations to assess strength, composition, and durability. We help industries prevent failures, improve product performance, and comply with safety regulations.",
+  },
+  {
+    id: 3,
+    image: "assuredQuality.jpeg",
+    section: "assured quality",
+    title: "Inspection Services",
+    description:
+      "Precision and accuracy in structural and material inspections are crucial for safety and compliance. Our inspection services include visual inspections, ultrasonic testing, radiographic testing, and other advanced techniques to detect defects and irregularities. By identifying issues early, we help businesses mitigate risks, prevent failures, and maintain operational efficiency.",
+  },
+  {
+    id: 4,
+    image: "skilledTraining.jpeg",
+    section: "skilled training",
+    title: "Technical Training",
+    description:
+      "Building a skilled workforce is key to maintaining industry excellence. We offer specialized training and certification programs in welding, non-destructive testing (NDT), and quality control. Our training programs are designed to equip professionals with practical knowledge, hands-on experience, and industry-recognized certifications, ensuring competency and compliance with global standards.",
+  },
+];
