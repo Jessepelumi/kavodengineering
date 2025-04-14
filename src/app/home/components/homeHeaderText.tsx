3f1205aee63937417dbaa8bfca68772865606bdb
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-14 20:41:24 +0100

    modified header and description texts

--- src/app/home/components/homeHeaderText.tsx
+++ src/app/home/components/homeHeaderText.tsx
@@ -1,12 +1,13 @@
 export const HomeHeaderText = () => {
   return (
     <article className="z-20 flex flex-col items-start gap-4">
-      <h1 className="text-[36px] leading-[42px] sm:text-[52px] md:text-[64px]  md:leading-[68px] font-bold max-w-[95%] md:max-w-[85%] lg:max-w-[80%] xl:max-w-[70%]">
-        Engineering Quality, Ensuring Integrity
-      </h1>
+      <div className="text-[36px] leading-[42px] sm:text-[52px] md:text-[64px]  md:leading-[68px] font-bold max-w-[95%] md:max-w-[85%] lg:max-w-[80%] xl:max-w-[70%]">
+        Engineering Quality. Ensuring Integrity.
+      </div>
+
       <p className="text-[14px] md:text-[16px] leading-[20px] md:leading-[24px] max-w-[80%] md:max-w-[70%] lg:max-w-[60%] xl:max-w-[45%]">
-        Leading experts in materials quality assurance and quality control
-        inspection for safer, stronger infrastructure.
+        Trusted experts in welding inspection, materials testing, and quality
+        assurance—building safer, stronger infrastructure.
       </p>
     </article>
   );
