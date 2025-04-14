78a75be23ad5bc293a1fd162bcf1d018f47181d5
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-14 23:19:13 +0100

    added new overlay

--- src/components/shared/overlay.tsx
+++ src/components/shared/overlay.tsx
@@ -3,3 +3,7 @@ export const Overlay = () => {
     <div className="absolute z-10 inset-0 bg-gradient-to-b from-transparent via-base-black via-85% to-base-black"></div>
   );
 };
+
+export const BlackOverlay = () => {
+  return <div className="absolute z-10 inset-0 bg-base-black-60"></div>;
+};
