663f09440db02c98a8389853c5787681e03035f2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-15 11:47:08 +0100

    created new overlay to simulate blur effect

--- src/components/shared/overlay.tsx
+++ src/components/shared/overlay.tsx
@@ -7,3 +7,9 @@ export const Overlay = () => {
 export const BlackOverlay = () => {
   return <div className="absolute z-10 inset-0 bg-base-black-60"></div>;
 };
+
+export const BlurOverlay = () => {
+  return (
+    <div className="absolute z-10 inset-0 bg-gradient-to-b from-transparent via-base-black-40 to-base-black via-60%"></div>
+  );
+};
